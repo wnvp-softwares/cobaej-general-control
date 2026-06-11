07540a669a9b1324cf1073d879cafbf0df6652d0
wnvp-softwares <wnvp115@gmail.com>
2026-06-10 20:48:51 -0600

    Reset del repositorio para cambio a arquitectura MVC

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    abrirNuevaVentana: (nombreArchivo) => ipcRenderer.send('abrir-nueva-ventana', nombreArchivo),
-    cerrarPrograma: () => ipcRenderer.send('cerrar-programa'),
-});
